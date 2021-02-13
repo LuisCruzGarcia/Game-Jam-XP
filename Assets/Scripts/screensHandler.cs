5e3b7c281e9dc4558c5eeb81a7d5cda0cd5bdd32
Euclid <euclidesmarquez98@outlook.com>
2021-02-13 14:17:25 -0600

    mecanicas2

---
+++ Assets/Scripts/screensHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screensHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject instructions, gameOver;
+    void Start()
+    {
+        instructions = GameObject.Find("instructions");
+        gameOver = GameObject.Find("GameOver");
+
+        
+        gameOver.SetActive(false);
+            
+    }
+
+    // Update is called once per frame
+    
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        instructions.SetActive(false);
+
+    }
+
+
+}
