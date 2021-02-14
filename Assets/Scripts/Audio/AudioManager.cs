cc6647907833d1e823e68d38cfe0fbc0b60dc5ce
Luis Cruz <luis.cruzgarcia98@gmail.com>
2021-02-14 12:31:00 -0600

    Collectible

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        this.InstantiateController();
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    private void InstantiateController()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (this != Instance)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.soundName == name);
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Collectibe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibe : MonoBehaviour
+{
+    public int puntuacion = 100;
+    public static SceneLoader Instance;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Instance.SubirPuntuacion(puntuacion);
+        }
+    }
+}
