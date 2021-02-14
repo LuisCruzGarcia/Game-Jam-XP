1a05e687b58cadb97df814ca8cd43555792d7e00
Euclid <euclidesmarquez98@outlook.com>
2021-02-14 12:28:15 -0600

    level1

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+
+    SceneLoader scene;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        scene = FindObjectOfType<SceneLoader>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            scene.LoadNextScene();
+        }
+    }
+}
